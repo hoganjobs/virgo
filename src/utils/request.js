5f69ce62b5707c5af17ffdf42264f7b650ec495b
hoganjobs <hongju110@126.com>
2021-03-26 11:24:18 +0800

    update vue+mock

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,35 +1,35 @@
 // /utils/request.js
-import axios from 'axios'
+import axios from "axios";
 
 // create an axios instance
 const service = axios.create({
-    baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-    // withCredentials: true, // send cookies when cross-domain requests
-    timeout: 5000 // request timeout
-})
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000, // request timeout
+});
 
 // request interceptor
 service.interceptors.request.use(
-    config => {
-        return config
-    },
-    error => {
-        // do something with request error
-        console.log(error) // for debug
-        return Promise.reject(error)
-    }
-)
+  (config) => {
+    return config;
+  },
+  (error) => {
+    // do something with request error
+    console.log(error); // for debug
+    return Promise.reject(error);
+  }
+);
 
 // response interceptor
 service.interceptors.response.use(
-    response => {
-        // const res = response.data;
-        return response;
-    },
-    error => {
-        console.log('err' + error) // for debug
-        return Promise.reject(error)
-    }
-)
+  (response) => {
+    // const res = response.data;
+    return response;
+  },
+  (error) => {
+    console.log("err" + error); // for debug
+    return Promise.reject(error);
+  }
+);
 
-export default service
+export default service;
