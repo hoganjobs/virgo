96619cba13399f339f9dff4aa4a3f6be9277d4b0
hoganjobs <hongju110@126.com>
2021-03-30 10:41:27 +0800

    update auto download

--- 01/vue-auto-router-cli/lib/init.js
+++ 01/vue-auto-router-cli/lib/init.js
@@ -3,10 +3,14 @@ const figlet = promisify(require('figlet'))
 const clear = require('clear')
 const chalk = require('chalk')
 const log = content => console.log(chalk.green(content))
+const {clone} = require('./download')
 
 module.exports = async name => {
   // 打印欢迎界面
   clear()
   const data = await figlet('KKB Welcome')
   log(data)
+
+  // 克隆项目
+  await clone('github:hoganjobs/hogan', name)
 }
