e3284ddd2fd79b76a03a76b8299bf3f5e3938e94
hoganjobs <hongju110@126.com>
2021-03-30 10:29:37 +0800

    update figlet

--- 01/vue-auto-router-cli/bin/kkb.js
+++ 01/vue-auto-router-cli/bin/kkb.js
@@ -6,8 +6,9 @@ program.version(require('../package').version)
 program
     .command('init <name>')
     .description('init project')
-    .action(name => {
-      console.log('init ' + name)
-    })
+    // .action(name => {
+    //   console.log('init ' + name)
+    // })
+    .action(require('../lib/init'))
 
 program.parse(process.argv)
