a1d22c8d3707119ce60bef748d65bd7004d456de
hoganjobs <hongju110@126.com>
2021-03-30 10:23:31 +0800

    update vue-auto-router-cli init

---
+++ 01/vue-auto-router-cli/bin/kkb.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log('cli...')
+const program = require('commander')
+program.version(require('../package').version)
+
+program
+    .command('init <name>')
+    .description('init project')
+    .action(name => {
+      console.log('init ' + name)
+    })
+
+program.parse(process.argv)
