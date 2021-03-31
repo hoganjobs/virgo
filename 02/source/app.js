25b911289e505fd878703569ededdda6eb7f178e
hoganjobs <hongju110@126.com>
2021-03-31 17:39:00 +0800

    update koa source middeware

--- 02/source/app.js
+++ 02/source/app.js
@@ -1,15 +1,34 @@
-const KKB = require('./kkb')
-const app = new KKB()
+const KKB = require("./kkb");
+const app = new KKB();
 
 // app.use((req,res) => {
 //   res.writeHead(200)
 //   res.end('hi kkb')
 // })
 
-app.use(ctx => {
-  ctx.body = 'hehe'
-})
+// app.use(ctx => {
+//   ctx.body = 'hehe'
+// })
+
+const delay = () => new Promise((resolve) => setTimeout(() => resolve(), 2000));
+
+app.use(async (ctx, next) => {
+  ctx.body = "1";
+  await next();
+  ctx.body += "5";
+});
+
+app.use(async (ctx, next) => {
+  ctx.body += "2";
+  await delay();
+  await next();
+  ctx.body += "4";
+});
+
+app.use(async (ctx, next) => {
+  ctx.body += "3";
+});
 
 app.listen(3000, () => {
-  console.log('listen at 3000')
-})
+  console.log("listen at 3000");
+});
