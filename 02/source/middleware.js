52ea99bb1b9e274ef1c88963364cbb22d5a47e97
hoganjobs <hongju110@126.com>
2021-03-31 17:14:16 +0800

    update koa middleware

---
+++ 02/source/middleware.js
@@ -0,0 +1,16 @@
+const add = (x, y) => x + y
+const square = z => z*z
+
+// const compose = (fn1, fn2) => (...args) => fn2(fn1(...args))
+const compose = (...[first, ...other]) => (...args) => {
+  let ret = first(...args)
+  other.forEach(fn => {
+    ret = fn(ret)
+  })
+  return ret
+}
+
+// const fn = (x, y) => square(add(x, y))
+const fn = compose(add, square, square)
+
+console.log(fn(1, 2))
