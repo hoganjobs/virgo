1a2a9c00d4200fbd398dd766bc15466f9bc44edd
hoganjobs <hongju110@126.com>
2021-03-31 17:04:25 +0800

    update koa source

--- 02/source/app.js
+++ 02/source/app.js
@@ -1,9 +1,13 @@
 const KKB = require('./kkb')
 const app = new KKB()
 
-app.use((req,res) => {
-  res.writeHead(200)
-  res.end('hi kkb')
+// app.use((req,res) => {
+//   res.writeHead(200)
+//   res.end('hi kkb')
+// })
+
+app.use(ctx => {
+  ctx.body = 'hehe'
 })
 
 app.listen(3000, () => {
