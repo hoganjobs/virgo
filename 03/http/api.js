0e1b0a22930a0e6de7844627d84c59a92673f506
hoganjobs <hongju110@126.com>
2021-04-01 13:58:30 +0800

    update 跨域 cookie

--- 03/http/api.js
+++ 03/http/api.js
@@ -4,6 +4,7 @@ http
   .createServer((req, res) => {
     const { method, url } = req;
     console.log("url: ", url, " method: ", method);
+    console.log('cookie', req.headers.cookie)
     if (method == "GET" && url == "/") {
       fs.readFile("./index.html", (err, data) => {
         res.setHeader("Content-Type", "text/html");
@@ -11,9 +12,13 @@ http
       });
     } else if (method == "GET" && url == "/api/users") {
       res.setHeader("Content-Type", "application/json");
+      res.setHeader('Access-Control-Allow-Credentials', 'true'); 
       res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.setHeader('Set-Cookie', 'cookie1=va222')
       res.end(JSON.stringify([{ name: "tom", age: 20 }]));
     } else if (method == "OPTIONS" && url == "/api/users") {
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.setHeader('Set-Cookie', 'cookie1=va222;')
       res.writeHead(200, {
         "Access-Control-Allow-Origin": "http://localhost:3000",
         "Access-Control-Allow-Headers": "X-Token,Content-Type",
