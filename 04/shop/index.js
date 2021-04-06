4758969f01a7db3df70e591b8224f8448f550368
hoganjobs <hongju110@126.com>
2021-04-06 11:01:23 +0800

    update shop

--- 04/shop/index.js
+++ 04/shop/index.js
@@ -80,4 +80,77 @@ router.delete("/admin/product/:id", async (ctx, next) => {
   ctx.body = { success: true };
 });
 
+// 查询购物车
+router.get("/cart", async (ctx) => {
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts();
+  ctx.body = { products };
+});
+
+// 添加购物车
+router.post("/cart", async (ctx) => {
+  const { body } = ctx.request;
+  const prodId = body.id;
+  let newQty = 1;
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts({
+    where: { id: prodId },
+  });
+  let product;
+  if (products.length > 0) {
+    product = products[0];
+  }
+  if (product) {
+    const oldQty = product.cartItem.quantity;
+    newQty = oldQty + 1;
+  } else {
+    product = await Product.findByPk(prodId);
+  }
+  await cart.addProduct(product, {
+    through: {
+      quantity: newQty,
+    },
+  });
+  ctx.body = { success: true };
+});
+
+// 添加订单
+router.post("/orders", async (ctx) => {
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts();
+  const order = await ctx.user.createOrder();
+  const result = await order.addProduct(
+    products.map((p) => {
+      p.orderItem = {
+        quantity: p.cartItem.quantity,
+      };
+      return p;
+    })
+  );
+  // 清空购物车
+  await cart.setProducts(null);
+  ctx.body = { success: true };
+});
+
+// 删除购物车
+router.delete("/cartItem/:id", async (ctx) => {
+  const id = ctx.params.id;
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts({
+    where: { id },
+  });
+  const product = products[0];
+  await product.cartItem.destroy();
+  ctx.body = { success: true };
+});
+
+// 查询订单
+router.get("/orders", async (ctx) => {
+  const orders = await ctx.user.getOrders({
+    include: ["products"],
+    order: [["id", "DESC"]],
+  });
+  ctx.body = { orders };
+});
+
 app.use(router.routes());
