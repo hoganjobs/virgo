117c2e2d4df1c5bd507de23db20775604aceace6
hoganjobs <hongju110@126.com>
2021-04-07 15:19:00 +0800

    update cookie

---
+++ 06/cookie.js
@@ -0,0 +1,15 @@
+const http = require('http')
+http.createServer((req, res) => {
+  if(req.url === '/favicon.ico') {
+    res.end('')
+    return
+  }
+
+  //观察cookie
+  console.log('cookie ', req.headers.cookie)
+
+  // 设置cookie
+  res.setHeader('Set-Cookie', 'cookie1=abc')
+  res.end('hello cookie')
+})
+.listen(3000)
