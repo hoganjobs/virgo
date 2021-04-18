4112bf7ccfa1640fcc818f1651e4fa56cec12936
Hogan <hongju110@126.com>
2021-04-18 19:35:47 +0800

    ts koa router decorate

---
+++ 09/ts-server/src/routes/user.ts
@@ -0,0 +1,16 @@
+import * as Koa from "koa";
+import { get, post } from "../utils/decors";
+
+const users = [{ name: "tom" }];
+export default class User {
+  @get("/users")
+  public list(ctx: Koa.Context) {
+    ctx.body = { ok: 1, data: users };
+  }
+
+  @post("/users")
+  public add(ctx: Koa.Context) {
+    users.push(ctx.request.body);
+    ctx.body = { ok: 1 };
+  }
+}
